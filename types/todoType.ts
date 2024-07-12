7b7e083e2cd6fd3e47396dd33098a7b21ee74203
Marek Včelák <vcelak.marek@gmail.com>
2024-07-12 21:07:11 +0200

    create todoType.ts

---
+++ types/todoType.ts
@@ -0,0 +1,21 @@
+export enum Priority {
+  Low = "Low",
+  Medium = "Medium",
+  High = "High",
+}
+
+export type TodoItem = {
+  id: string
+  title: string
+  description?: string
+  completed: boolean
+  priority: Priority
+  dueDate?: string
+  tags?: string[]
+}
+
+export type TodoList = {
+  id: string
+  name: string
+  items: TodoItem[]
+}
