fdf38ad2836f81d373aaa4d6c6545f443e2d1e93
Marek Včelák <vcelak.marek@gmail.com>
2024-07-12 21:03:16 +0200

    define global variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "purple": "var(--purple)",
+        "purple-hover": "var(--purple-hover)",
+        "red":"var(--red)",
+        "red-hover":"var(--red-hover)",
+        "dark-blue":"var(--dark-blue)",
+        "blue":"var(--blue)",
+        "light-blue":"var(--light-blue)",
+        "light-grey":"var(--light-grey)",
+        "grey":"var(--grey)",
+        "black":"var(--black)",
+        "black2":"var(--black2)",
+        "light-bg":"var(--light-bg)"
       },
     },
   },
