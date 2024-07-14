603e57ea1d612cbdd33baa6fe8adce4978cfb5a0
VcelakMarek <vcelak.marek@gmail.com>
2024-07-14 20:57:43 +0200

    add edit, delete buttons, add priority

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { useState } from "react"
-import Link from "next/link"
-import { updateItem } from "api/todoItemAPI"
+import { updateItem, deleteItem } from "api/todoItemAPI"
 import type { TodoItem } from "types/todoType"
+import Button from "./Button"
+import Priority from "./Priority"
 
 type Props = {
   itemData: TodoItem
@@ -19,28 +20,34 @@ const TodoItem = ({ itemData }: Props) => {
     })
   }
 
+  const handleDelete = async () => {
+    await deleteItem(itemData.id)
+  }
+
   return (
-    <Link
-      href={`/list/${itemData.todoListId}/edit/${itemData.id}`}
-      className="mb-4 flex h-[72px] w-[65%] items-center justify-around rounded-lg border-[1.5px] border-transparent bg-white px-[2%] drop-shadow hover:border-[1.5px] hover:border-[#7C5DFA]"
-    >
-      <div className="mb-4 flex h-[72px] w-[65%] items-center justify-around rounded-lg border-[1.5px] border-transparent bg-white px-[2%] drop-shadow hover:border-[1.5px] hover:border-[#7C5DFA]">
-        <h3 className="basis-2/12">
-          <span className="font-bold text-grey"></span>
-          {itemData.completed}
-        </h3>
-        <input
-          id={itemData.id}
-          type="checkbox"
-          checked={isChecked}
-          onChange={handleChange}
-        />
-        <h2 className="basis-2/12">Due {itemData.dueDate}</h2>
-        <h2 className="basis-2/12">{itemData.title}</h2>
-        <h4 className="basis-2/12">{itemData.priority}</h4>
-        <div className="flex basis-2/12 items-center gap-5"></div>
+    <div className="mb-4 flex h-[72px] w-[65%] items-center justify-around rounded-lg border-[1.5px] border-transparent bg-white px-[2%] drop-shadow hover:border-[1.5px]">
+      <input
+        id={itemData.id}
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+      />
+      <h4 className="basis-2/12">{itemData.title}</h4>
+      <h2 className="basis-2/12">Due {itemData.dueDate}</h2>
+      <Priority priority={itemData.priority} />
+      <div className="flex basis-2/12 items-center gap-4">
+        <Button
+          color="grey"
+          link={`/list/${itemData.todoListId}/edit/${itemData.id}`}
+        >
+          Edit
+        </Button>
+        <Button color="red" onClick={handleDelete}>
+          {" "}
+          Delete
+        </Button>
       </div>
-    </Link>
+    </div>
   )
 }
 
