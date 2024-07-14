4d89273fc0654f2e3cf9c2f09b64b4097979c092
VcelakMarek <vcelak.marek@gmail.com>
2024-07-14 15:13:25 +0200

    add getItems, getItem, updateItem, addItem

--- app/api/todoItemAPI.ts
+++ app/api/todoItemAPI.ts
@@ -2,13 +2,48 @@ import { AxiosResponse, isAxiosError } from "axios"
 import { GET, POST, PUT, DELETE } from "connectors/fetch"
 import type { TodoItem } from "types/todoType"
 
-const url = "todoItems"
+const URL = "todoLists"
 
-export const getTodoItems: () => Promise<AxiosResponse<
-  TodoItem[]
-> | null> = async () => {
+export const getItems: (
+  id: string,
+) => Promise<AxiosResponse<TodoItem[]> | null> = async (id) => {
   try {
-    return await GET<TodoItem[]>(url)
+    return await GET<TodoItem[]>(`${URL}/${id}/todoItems`)
+  } catch (e) {
+    return handleError(e)
+  }
+}
+
+export const getItem: (
+  id: string,
+) => Promise<AxiosResponse<TodoItem[]> | null> = async (id) => {
+  try {
+    return await GET<TodoItem>(`/todoItems/${id}`)
+  } catch (e) {
+    return handleError(e)
+  }
+}
+
+export const updateItem: (
+  id: string,
+  data: Partial<TodoItem>,
+) => Promise<AxiosResponse<TodoItem> | null> = async (
+  id,
+  data: Partial<TodoItem>,
+) => {
+  console.log(data)
+  try {
+    return await PUT(`todoItems/${id}/`, data as TodoItem)
+  } catch (e) {
+    return handleError(e)
+  }
+}
+
+export const addItem: (
+  data: TodoItem,
+) => Promise<AxiosResponse<TodoItem> | null> = async (data: TodoItem) => {
+  try {
+    return await POST("todoItems/", data)
   } catch (e) {
     return handleError(e)
   }
