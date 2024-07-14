82dda7fbab97b2e2e2fa4b4341bf72452400663a
VcelakMarek <vcelak.marek@gmail.com>
2024-07-14 20:49:02 +0200

    add addList

--- app/api/todoListAPI.ts
+++ app/api/todoListAPI.ts
@@ -2,13 +2,23 @@ import { AxiosResponse, isAxiosError } from "axios"
 import { GET, POST, PUT, DELETE } from "connectors/fetch"
 import type { TodoList } from "types/todoType"
 
-const url = "todoLists"
+const URL = "todoLists"
 
 export const getTodoLists: () => Promise<AxiosResponse<
   TodoList[]
 > | null> = async () => {
   try {
-    return await GET<TodoList[]>(url)
+    return await GET<TodoList[]>(URL)
+  } catch (e) {
+    return handleError(e)
+  }
+}
+
+export const addList: (
+  data: TodoList,
+) => Promise<AxiosResponse<TodoList> | null> = async (data: TodoList) => {
+  try {
+    return await POST("todoLists", data)
   } catch (e) {
     return handleError(e)
   }
