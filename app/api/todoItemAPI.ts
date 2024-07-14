576401051216d7bc5bc0894b47a159ee4b4003da
VcelakMarek <vcelak.marek@gmail.com>
2024-07-14 20:47:33 +0200

    add deleteItem

--- app/api/todoItemAPI.ts
+++ app/api/todoItemAPI.ts
@@ -2,13 +2,11 @@ import { AxiosResponse, isAxiosError } from "axios"
 import { GET, POST, PUT, DELETE } from "connectors/fetch"
 import type { TodoItem } from "types/todoType"
 
-const URL = "todoLists"
-
 export const getItems: (
   id: string,
 ) => Promise<AxiosResponse<TodoItem[]> | null> = async (id) => {
   try {
-    return await GET<TodoItem[]>(`${URL}/${id}/todoItems`)
+    return await GET<TodoItem[]>(`todoLists/${id}/todoItems`)
   } catch (e) {
     return handleError(e)
   }
@@ -18,7 +16,7 @@ export const getItem: (
   id: string,
 ) => Promise<AxiosResponse<TodoItem[]> | null> = async (id) => {
   try {
-    return await GET<TodoItem>(`/todoItems/${id}`)
+    return await GET<TodoItem>(`todoItems/${id}`)
   } catch (e) {
     return handleError(e)
   }
@@ -33,7 +31,7 @@ export const updateItem: (
 ) => {
   console.log(data)
   try {
-    return await PUT(`todoItems/${id}/`, data as TodoItem)
+    return await PUT(`todoItems/${id}`, data as TodoItem)
   } catch (e) {
     return handleError(e)
   }
@@ -43,7 +41,17 @@ export const addItem: (
   data: TodoItem,
 ) => Promise<AxiosResponse<TodoItem> | null> = async (data: TodoItem) => {
   try {
-    return await POST("todoItems/", data)
+    return await POST("todoItems", data)
+  } catch (e) {
+    return handleError(e)
+  }
+}
+
+export const deleteItem: (
+  id: string,
+) => Promise<AxiosResponse<TodoItem> | null> = async (id: string) => {
+  try {
+    return await DELETE(`todoItems/${id}`)
   } catch (e) {
     return handleError(e)
   }
