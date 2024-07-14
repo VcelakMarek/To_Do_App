2e49a276bacc77d65889851cb99cb7dc01172688
VcelakMarek <vcelak.marek@gmail.com>
2024-07-14 15:20:48 +0200

    fix Link

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -8,12 +8,10 @@ type Props = {
 
 const ListItem = ({ listData }: Props) => {
   console.log(listData.name)
+
   return (
     <Link
-      href={{
-        pathname: `/todoList`,
-        query: { todoList: listData.name },
-      }}
+      href={`/list/${listData.id}`}
       className="mb-4 flex h-[72px] w-[65%] items-center justify-around rounded-lg border-[1.5px] border-transparent bg-white px-[2%] drop-shadow hover:border-[1.5px] hover:border-[#7C5DFA]"
     >
       <h3>{listData.name}</h3>
