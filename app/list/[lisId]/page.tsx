aaca7c003b6dcd501cdbdd7e174a6a02ef38d1cb
VcelakMarek <vcelak.marek@gmail.com>
2024-07-15 08:38:23 +0200

    refactor code

--- app/list/[lisId]/page.tsx
+++ app/list/[lisId]/page.tsx
@@ -8,8 +8,10 @@ type Props = {
 
 const TodoList = async ({ params }: Props) => {
   const response = await getItems(params.lisId)
+
   if (!response?.data) return <div>No data</div>
-  const todoItems = response?.data
+
+  const todoItems = response.data
 
   return (
     <div className="h-screen bg-light-bg">
@@ -18,7 +20,7 @@ const TodoList = async ({ params }: Props) => {
           <header className="w-800px mt-16 flex h-14 flex-row items-start justify-around">
             <div>
               <h1>To Do</h1>
-              <h2>There are {todoItems?.length} to-do's</h2>
+              <h2>There are {todoItems.length} to-do's</h2>
             </div>
             <div className="flex gap-10">
               <Button color="purple" link={`/list/${params.lisId}/newItem`}>
@@ -27,14 +29,13 @@ const TodoList = async ({ params }: Props) => {
             </div>
           </header>
           <div className="mt-16 grid place-items-center">
-            <div className="mb-2 flex w-[65%] items-start gap-24 pl-[85px]">
-              <h4>Name</h4>
-              <h4>Due Date</h4>
-              <h4>Priority</h4>
-            </div>
-            {todoItems?.map((todoItem) => (
-              <TodoItem itemData={todoItem} key={todoItem.id} />
-            ))}
+            {Array.isArray(response.data) && todoItems.length > 0 ? (
+              todoItems.map((todoItem) => (
+                <TodoItem itemData={todoItem} key={todoItem.id} />
+              ))
+            ) : (
+              <p>List is empty</p>
+            )}
           </div>
         </main>
       </div>
