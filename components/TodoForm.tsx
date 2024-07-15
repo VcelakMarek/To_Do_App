34fe3c5f916011952116734e48d09a89a7d0a0d4
VcelakMarek <vcelak.marek@gmail.com>
2024-07-15 08:28:21 +0200

    refactor, fix onSubmit

--- components/TodoForm.tsx
+++ components/TodoForm.tsx
@@ -1,12 +1,12 @@
 "use client"
 import { FC } from "react"
-import { Form } from "react-final-form"
-import { FormApi } from "final-form"
 import { useRouter } from "next/navigation"
-import { updateItem, addItem } from "api/todoItemAPI"
+import { FormApi } from "final-form"
+import { Form } from "react-final-form"
 import FormInput from "components/FormInput"
 import Button from "components/Button"
-import type { TodoList, TodoItem } from "types/todoType"
+import { updateItem, addItem } from "api/todoItemAPI"
+import type { TodoItem } from "types/todoType"
 
 type Props = {
   form?: FormApi<FormData>
@@ -15,111 +15,61 @@ type Props = {
 }
 
 const TodoForm: FC<Props> = ({ formValues, listId }) => {
-  const { push } = useRouter()
+  const router = useRouter()
 
-  const onSubmit = (values: TodoItem) => {
+  const onSubmit = async (values: TodoItem) => {
     const newItem: TodoItem = {
       ...values,
+      description: values.description ?? "",
       completed: false,
-      todoListId: listId,
+      priority: values.priority ?? "Low",
+      todoListId: listId ?? values.todoListId,
     }
 
-    formValues ? updateItem(formValues.id, values) : addItem(newItem)
-    push(`/list/${formValues?.todoListId ?? listId}`)
+    formValues
+      ? await updateItem(formValues.id, values)
+      : await addItem(newItem)
+    router.back()
+    setTimeout(() => {
+      router.refresh()
+    }, 200)
   }
 
-  if (formValues) {
-    return (
-      <>
-        <div className="bg-white-background grid h-full w-screen place-items-center p-10">
-          <h1>Edit Item</h1>
-          <Form
-            id="editItem"
-            className="w-screen"
-            onSubmit={onSubmit}
-            initialValues={formValues ?? null}
-            render={({ handleSubmit }) => (
-              <form onSubmit={handleSubmit}>
-                <main className="bg-white-background flex h-screen w-screen flex-wrap pt-10">
-                  <section className="mx-auto flex w-3/5 flex-col gap-1">
-                    <FormInput inputName="Name" id="title" />
-
-                    <FormInput inputName="Description" id="description" />
-                    <div className="mt-10 flex gap-10">
-                      <FormInput
-                        inputName="Due Date"
-                        size="small"
-                        inputType="date"
-                        id="dueDate"
-                      />
-
-                      <FormInput
-                        id="priority"
-                        inputName="Priority"
-                        size="small"
-                        inputType="select"
-                        selectValues={["Low", "Medium", "High"]}
-                      />
-                    </div>
-
-                    <div>
-                      <Button submit color="purple">
-                        Save changes
-                      </Button>
-                    </div>
-                  </section>
-                </main>
-              </form>
-            )}
-          />
-        </div>
-      </>
-    )
-  }
   return (
-    <>
-      <div className="bg-white-background grid h-full w-screen place-items-center p-10">
-        <h1>New Item</h1>
-        <Form
-          id="newItem"
-          className="w-screen"
-          onSubmit={onSubmit}
-          render={({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <main className="bg-white-background flex h-screen w-screen flex-wrap pt-10">
-                <section className="mx-auto flex w-3/5 flex-col gap-1">
-                  <FormInput inputName="Name" id="title" />
+    <div className="bg-white-background grid h-screen w-screen place-items-center pt-10">
+      <h1>{formValues ? "Edit Item" : "New Item"}</h1>
+      <Form
+        id={formValues ? "editItem" : "newItem"}
+        className="w-screen"
+        onSubmit={onSubmit}
+        initialValues={formValues ?? null}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <main className="bg-white-background flex h-screen w-screen flex-wrap pt-10">
+              <section className="mx-auto flex flex-col gap-1">
+                <FormInput inputName="Name" id="title" />
 
-                  <FormInput inputName="Description" id="description" />
-                  <div className="mt-10 flex gap-10">
-                    <FormInput
-                      inputName="Due Date"
-                      size="small"
-                      inputType="date"
-                      id="dueDate"
-                    />
+                <FormInput inputName="Description" id="description" />
 
-                    <FormInput
-                      id="priority"
-                      inputName="Priority"
-                      size="small"
-                      inputType="select"
-                      selectValues={["Low", "Medium", "High"]}
-                    />
-                  </div>
+                <FormInput
+                  id="priority"
+                  inputName="Priority"
+                  size="small"
+                  inputType="select"
+                  selectValues={["Low", "Medium", "High"]}
+                />
 
-                  <div>
-                    <Button submit color="purple">
-                      Add Item
-                    </Button>
-                  </div>
-                </section>
-              </main>
-            </form>
-          )}
-        />
-      </div>
-    </>
+                <div>
+                  <Button submit color="purple">
+                    {formValues ? "Save changes" : "Add Item"}
+                  </Button>
+                </div>
+              </section>
+            </main>
+          </form>
+        )}
+      />
+    </div>
   )
 }
 
